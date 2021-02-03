53aaf31557b5c4ee1f46285e18ee850987c69a37
Alexandre Gros <xelaflash@gmail.com>
2021-02-03 09:22:43 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: "passion-doner",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "",
+        dataset: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
